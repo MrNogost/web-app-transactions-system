c2a542a8dcb7628439e71b32f850e768c42ad91a
du5dx <du5dx@DESKTOP-G3RVU4I>
2025-01-11 11:55:39 -0700

    V0

---
+++ src/main/java/com/portfolio/web_app_transactions_system/WebAppTransactionsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.web_app_transactions_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppTransactionsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppTransactionsSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/web_app_transactions_system/WebAppTransactionsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.web_app_transactions_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppTransactionsSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
